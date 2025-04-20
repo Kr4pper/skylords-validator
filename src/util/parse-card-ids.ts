40b116b6109813a7c4153868a6c0733e60bd69cf
Kr4pper <Kr4pper@github.com>
2025-04-20 12:18:58 +0200

    added card id file parsing

---
+++ src/util/parse-card-ids.ts
@@ -0,0 +1,20 @@
+import {readFileSync, writeFileSync} from 'fs';
+
+const cardIdsFile = readFileSync('src/data/card-ids.txt').toString().split('\r\n');
+console.log('Input file read, number of lines is', cardIdsFile.length);
+
+const cards = new Map<string, number>();
+
+let idx = 0;
+while (idx < cardIdsFile.length - 1) {
+    const id = cardIdsFile[idx++].match(/(\d+)/)[0];
+    const name = cardIdsFile[idx++].match(/(\w+)/)[0];
+    cards.set(name, +id);
+}
+
+const result = `export enum CardIds {
+    ${[...cards.entries()].map(([name, id]) => `${name} = ${id},`).join('\n ')}
+}`;
+
+writeFileSync('src/data/card-ids.ts', result);
+console.log('Output file written with', cards.size, 'entries');
